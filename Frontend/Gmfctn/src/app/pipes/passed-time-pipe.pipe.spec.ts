13b468c7f7b8778ccd1f4e99edf0136f86b3cf8a
Bea-tyxz <pkoval011@gmail.com>
2021-03-30 03:52:11 +0300

    Refactoring, continueing

---
+++ Frontend/Gmfctn/src/app/pipes/passed-time-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PassedTimePipePipe } from './passed-time-pipe.pipe';
+
+describe('PassedTimePipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PassedTimePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
