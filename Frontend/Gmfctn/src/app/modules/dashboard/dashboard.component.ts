9bc8a21db631a62e34297fc31870bf8f7d06cb65
Bea-tyxz <pkoval011@gmail.com>
2021-03-31 22:17:39 +0300

    Change some css in dashboard

--- Frontend/Gmfctn/src/app/modules/dashboard/dashboard.component.ts
+++ Frontend/Gmfctn/src/app/modules/dashboard/dashboard.component.ts
@@ -6,7 +6,19 @@ import '../../models/user';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public height = 0;
+  public margin = '';
+  public user: User = {name: 'Aezakmi', surname: 'Houston', total: 0, icon: ''};
 
-  user: User = {name: 'Aezakmi', surname: 'Houston', total: 0, icon: ''};
+  ngOnInit(): void {
+    this.calculateSize();
+}
+
+  calculateSize(): void {
+
+    this.height = ( document.body.clientHeight - 200 ) / 7;
+    this.margin = this.height / 7 + '';
+
+  }
 }
